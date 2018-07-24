4ad8221a54aaffbb19e5735dda3178e24f97887c
Francisco <fjgutierrez112@gmail.com>
2018-07-24 09:33:50 -0400

    created about component

---
+++ src/components/about/about.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class about extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is my about me page </h2>
+      </div>
+    );
+  }
+}
+
+export default about;
