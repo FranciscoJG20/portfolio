f124343d9bb3edf23318e1d1941b417a7dc78c7a
Francisco <fjgutierrez112@gmail.com>
2018-07-24 09:33:26 -0400

    set up react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/app/app";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
+
+//The Virtual DOM is a Javascript representation of the actual DOM.
+//The virtual DOM is a staging area for changes that will eventually be implemented.
