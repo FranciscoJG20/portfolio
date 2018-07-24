7c23bff77d5e58c227d8c11632cb62b9e5493e4f
Francisco <fjgutierrez112@gmail.com>
2018-07-24 14:29:33 -0400

    building out app component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import "./app.css";
+import About from "../about/about";
+import Projects from "../projects/projects";
+import { Link, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header" />
+      <div className="app">
+        {/* <Projects /> */}
+        <h1>Francisco Vince Gutierrez</h1>
+        <nav className="navbar">
+          <div>
+            <Link to="/about">About</Link>
+          </div>
+          <div>
+            <Link to="/projects">Projects</Link>
+          </div>
+        </nav>
+        <Route path="/projects" component={Projects} />
+        <Route path="/about" component={About} />
       </div>
     );
   }
