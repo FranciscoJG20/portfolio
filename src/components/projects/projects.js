7f930146579f0451c0d31d0845f7a8e471fb0229
Francisco <fjgutierrez112@gmail.com>
2018-07-26 17:12:24 -0400

    included projects

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -5,7 +5,36 @@ class Projects extends Component {
   render() {
     return (
       <div>
-        <h2> This is my projects page </h2>
+        <div className="flexbox-container">
+          <div className="flex-item">
+            <a
+              href="https://franciscojg20.github.io/rugratsgame/"
+              target="_blank"
+            >
+              <img
+                src="project1.png"
+                alt="screenshot of a Rugrats themed memory card game"
+              />
+            </a>
+          </div>
+          <div class="flex-item">
+            <a href="https://biasreporting.herokuapp.com" target="_blank">
+              <img
+                src="project2.png"
+                alt="screenshot of a bias reporting application"
+              />
+            </a>
+          </div>
+
+          <div class="flex-item">
+            <a href="http://fitnessfriends.surge.sh/" target="_blank">
+              <img
+                src="project3.png"
+                alt="screenshot of a fitness social network"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
