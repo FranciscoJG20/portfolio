011c8d013c9d1ed66c74a92e20e4e6ef64e098fa
Francisco <fjgutierrez112@gmail.com>
2018-07-26 17:12:49 -0400

    made changes to how resume appears when rendering

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -8,7 +8,7 @@ class Resume extends Component {
         <iframe
           id="resume"
           src="https://docs.google.com/document/d/e/2PACX-1vQ-VgF6RiNeFgGqdmSgppLcj4qSYqOXtLAZ7SEom0ZT68I6j2-5xcNKK-Vr4WyG8cI6JDiHq8rHiOsi/pub?embedded=true"
-          style={{ width: "550px", height: "500px", border: "none" }}
+          style={{ width: "900px", height: "500px", border: "none" }}
         />
       </div>
     );
